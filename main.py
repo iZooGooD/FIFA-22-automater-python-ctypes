4438e096f973fd0bd637ff5371ca95bb05bf4a6e
iZooGooD <lakshmanchoudhary020@gmail.com>
2021-03-08 12:00:00 +0530

    add initial imports and constants keys

---
+++ main.py
@@ -0,0 +1,13 @@
+import ctypes
+import time
+import threading
+import pyautogui
+
+# Constants for keys
+ENTER_KEY = 0x1C
+S_KEY = 0x1F
+ESC_KEY = 0x01
+ARROW_DOWN_KEY = 0xD0
+ARROW_UP_KEY = 0xC8
+ARROW_LEFT_KEY = 0xCB
+ARROW_RIGHT_KEY = 0xCD
